a7512b3b2f6d58e05ffc124bf9f1e7739aae3d6c
Paolo Cargnin <paolocargnin@boom.ink>
2021-10-04 12:30:47 +0200

    lesson3

--- script.js
+++ script.js
@@ -14,9 +14,22 @@ Vue.createApp({
             // the this.giacomoHp variable
             // this.giacomoHp = .......
             this.matteoHp = this.matteoHp - 10
+
+            if ( this.matteoHp <= 0){
+                // do something when it is dead
+            }
         },
         hitGiacomo() {
+            if (this.giacomoHp < 1){
+                return false
+            }
             this.giacomoHp = this.giacomoHp - 10
+            if ( this.giacomoHp <= 0){
+                // do something when it is dead
+                // YOU CAN LIKE FOR EXAPLE PLAY A ASOUND
+                console.log(this.giacomoHp)
+            }
+
         },
     }
 }).mount('.container')
